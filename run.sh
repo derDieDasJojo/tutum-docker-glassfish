1fffc17afd54b352938c203cbfe5085a24eacc01
derDieDasJojo <spamort@web.de>
2014-08-22 21:03:31 +0200

    enable-secure-admin added

--- run.sh
+++ run.sh
@@ -6,7 +6,11 @@ asadmin start-domain
 if [ ! -f /.glassfish_admin_password_changed ]; then
     /change_admin_password.sh
 fi
+
 echo "=> Restarting Glassfish server"
 asadmin stop-domain
 echo "=> Starting and running Glassfish server"
 asadmin start-domain -w
+
+echo "=> Enabeling secure-admin"
+asadmin --host localhost --port 4848 enable-secure-admin
